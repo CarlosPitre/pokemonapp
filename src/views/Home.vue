14f2c340b95496e0d06cebd5b113a61f844da9bc
Carlos Pitre <carlosjpitre@gmail.com>
2021-05-08 16:20:49 -0500

    Edit Home View with required designs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="image-logo" src="../assets/pikachu-home.svg" >
+    <h1 class="title">Welcome to Pokédex</h1>
+    <p class="description">
+      The digital encyclopedia created by Professor Oak is an
+      invaluable tool to Trainers in the Pokémon world.
+    </p>
+    <button-primary text="Get started" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ButtonPrimary from '@/components/ButtonPrimary.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+    ButtonPrimary,
   },
 });
 </script>
+<style scoped lang='scss'>
+  .home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: inherit;
+    flex-direction: column;
+  }
+  .title {
+    font-style: normal;
+    font-weight: bold;
+    font-size: 26px;
+    line-height: 31px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    color: #353535;
+  }
+  .description {
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 150%;
+    text-align: center;
+    color: #5E5E5E;
+    max-width: 570px;
+  }
+</style>
