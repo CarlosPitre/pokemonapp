187b70ebb5519972d207e9e88168c93f85cfeade
Carlos Pitre <carlosjpitre@gmail.com>
2021-05-09 10:52:12 -0500

    Create PokemonList Component

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-for="(pokemon, index) in pokemonList" :key="index">
+   <pokemon-item :name="pokemon.name" :favorite="pokemon.favorite" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { MyPokemonList } from '@/types/GetPokemons';
+import PokemonItem from '@/components/PokemonItem.vue';
+
+export default defineComponent({
+  name: 'PokemonListComponent',
+  components: {
+    PokemonItem,
+  },
+  props: {
+    pokemonList: {
+      type: Array as PropType<Array<MyPokemonList>>,
+      default: () => [],
+    },
+  },
+});
+</script>
+
+<style></style>
