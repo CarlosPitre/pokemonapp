88110a76125b0b7f19c742bb14936cecdbc7c5a4
Carlos Pitre <carlosjpitre@gmail.com>
2021-05-09 10:51:06 -0500

    Change Name Interface PokemonClient

--- src/clients/pokemonClient.ts
+++ src/clients/pokemonClient.ts
@@ -2,14 +2,14 @@ import HttpProvider from '@/api/HttpProvider';
 import GetPokemons from '@/types/GetPokemons';
 import Pokemon from '@/types/Pokemon';
 
-export interface PokemonClient {
+export type PokemonClientInterface = {
   getPokemons(url?: string): Promise<GetPokemons>;
   getPokemonByName(pokemonName: string): Promise<Pokemon>;
 }
 
 const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 
-function buildPokemonClient(httpProvider: HttpProvider): PokemonClient {
+function buildPokemonClient(httpProvider: HttpProvider): PokemonClientInterface {
   return {
     getPokemons(url?: string) {
       return httpProvider.get<GetPokemons>(url || BASE_URL);
