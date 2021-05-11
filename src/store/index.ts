c3a12d288d70b447d45f1fc6ff748307c2cc1f29
Carlos Pitre <carlosjpitre@gmail.com>
2021-05-10 22:11:55 -0500

    Define store, mutations and actions for manage favores

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,26 @@
+import mockFavorities from '@/clients/Mock';
+import { MyPokemonList } from '@/types/GetPokemons';
 import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    myPokemons: mockFavorities,
   },
   mutations: {
+    addFavorite(state, newPokemon: MyPokemonList) {
+      state.myPokemons = [...state.myPokemons, { ...newPokemon, favorite: true }];
+    },
+    removeFavorite(state, pokemonName: string) {
+      state.myPokemons = state.myPokemons.filter((pokemon) => pokemon.name !== pokemonName);
+    },
   },
   actions: {
+    addFavoriteAction({ commit }, newPokemon: MyPokemonList) {
+      commit('addFavorite', newPokemon);
+    },
+    removeFavoriteAction({ commit }, pokemonName: string) {
+      commit('removeFavorite', pokemonName);
+    },
   },
   modules: {
   },
